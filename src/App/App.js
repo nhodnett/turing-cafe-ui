ec33e6ddf2b8ab2e4709090fbd1c5ce0315672ea
Nathan Hodnett <nathanhodnett@gmail.com>
2022-07-20 09:29:30 -0600

    Add componentDidMount, data coming through

--- src/App/App.js
+++ src/App/App.js
@@ -2,14 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      reservations: []
+    }
+
+  }
+
+  componentDidMount = () => {
+    console.log('Did Mount')
+    fetch(`http://localhost:3001/api/v1/reservations`)
+    .then(response => response.json())
+    .then(reservations => this.setState({ reservations }))
+  }
+
   render() {
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
-        <div className='resy-form'>
+        <div className='reservation-form'>
 
         </div>
-        <div className='resy-container'>
+        <div className='reservation-container'>
           
         </div>
       </div>
